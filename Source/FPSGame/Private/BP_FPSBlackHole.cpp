ea976e8d2caa08187518c9ba42effeea11445caf
OnionStark <244081385@qq.com>
2018-09-13 23:50:42 +0800

    项目初始化

---
+++ Source/FPSGame/Private/BP_FPSBlackHole.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BP_FPSBlackHole.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+ABP_FPSBlackHole::ABP_FPSBlackHole()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = MeshComp;
+
+	InnerSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("InnerSphereComp"));
+	InnerSphereComponent->SetSphereRadius(100);
+	InnerSphereComponent->SetupAttachment(MeshComp);
+
+	InnerSphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ABP_FPSBlackHole::OverlapInnerSphere);
+
+	OuterSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("OuterSphereComp"));
+	OuterSphereComponent->SetSphereRadius(3000);
+	OuterSphereComponent->SetupAttachment(MeshComp);
+}
+
+// Called when the game starts or when spawned
+void ABP_FPSBlackHole::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABP_FPSBlackHole::OverlapInnerSphere(UPrimitiveComponent * OverlappedComponent, AActor* OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if (OtherActor)
+	{
+		OtherActor->Destroy();
+	}
+}
+
+// Called every frame
+void ABP_FPSBlackHole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	TArray<UPrimitiveComponent*> OverlappingComps;
+	OuterSphereComponent->GetOverlappingComponents(OverlappingComps);
+
+	for (int32 i = 0; i < OverlappingComps.Num(); i++)
+	{
+		UPrimitiveComponent* PrimComp = OverlappingComps[i];
+		if (PrimComp && PrimComp->IsSimulatingPhysics()) {
+
+			const float SphereRadius = OuterSphereComponent->GetScaledSphereRadius();
+			const float ForceStrength = -2000;
+
+			PrimComp->AddRadialForce(GetActorLocation(), SphereRadius, ForceStrength, ERadialImpulseFalloff::RIF_Constant, true);
+		}
+	}
+}
+
